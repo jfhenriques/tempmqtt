ec3f05fdae0763e696c1344ff076ad779e0df209
João Henriques <joaoh88@gmail.com>
2020-07-26 13:44:40 +0100

    Added support to read path directory size using du suprocess

--- SystemReading.py
+++ SystemReading.py
@@ -4,8 +4,7 @@ import psutil
 from datetime import datetime, timezone
 import tzlocal
 from Reading import Reading
-import json
-
+import subprocess
 
 RPI_TEMP_FILE = '/sys/class/thermal/thermal_zone0/temp'
 RPI_THRO_FILE = '/sys/devices/platform/soc/soc:firmware/get_throttled'
@@ -13,16 +12,21 @@ RPI_THRO_FILE = '/sys/devices/platform/soc/soc:firmware/get_throttled'
 
 
 def utc_to_local(utc_dt):
-    return utc_dt.replace(tzinfo=timezone.utc).astimezone(tz=None)
+   return utc_dt.replace(tzinfo=timezone.utc).astimezone(tz=None)
 
 def get_cpu_temp():
-    with open(RPI_TEMP_FILE, "r") as f:
-        return round(int(f.readline()) / 1000, 1)
+   with open(RPI_TEMP_FILE, "r") as f:
+      return round(int(f.readline()) / 1000, 1)
 
 def get_throttled():
-    _throttled = open(RPI_THRO_FILE, 'r').read()[:-1]
-    return _throttled[:4]
+   _throttled = open(RPI_THRO_FILE, 'r').read()[:-1]
+   return _throttled[:4]
 
+def get_dir_size(path):
+   try:
+      return int(subprocess.check_output(['du','-sx', path]).split()[0].decode('utf-8'))
+   except:
+      return None
 
 class SystemReading(Reading):
 
@@ -35,13 +39,23 @@ class SystemReading(Reading):
 
 class SystemSensor():
 
-   def readData(self, isRPI = True, reading = SystemReading()):
+   def __init__(self, isRPI = True, dirList = [], dirReadMinSeconds = 600):
+      self._isRPI = isRPI
+      self._dirList = dirList
+     
+      self._dirReadMinSeconds = dirReadMinSeconds
+      self._dirLastCheckDate = None
+      self._dirCacheSizes = {} 
+
+      self._lastBoot = utc_to_local(datetime.utcfromtimestamp(psutil.boot_time())).isoformat()
+
+   def readData(self, reading = SystemReading()):
 
       reading.processor_use = round(psutil.cpu_percent(interval=None))
       reading.load_5 = round(os.getloadavg()[1], 2)
       reading.load_15 = round(os.getloadavg()[2], 2)
 
-      if isRPI:
+      if self._isRPI:
          reading.throttled = get_throttled()
 
          reading.cpu_temperature = get_cpu_temp()
@@ -52,7 +66,28 @@ class SystemSensor():
 
       reading.disk_use_percent = psutil.disk_usage('/').percent
 
-      reading.last_boot = utc_to_local(datetime.utcfromtimestamp(psutil.boot_time())).isoformat()
+      reading.last_boot = self._lastBoot
+
+      self._readDirSizes(reading)
 
       return reading 
 
+   def _readDirSizes(self, reading):
+
+      if self._dirList:
+
+         if self._dirLastCheckDate is None or int((datetime.now() - self._dirLastCheckDate).total_seconds()) >= self._dirReadMinSeconds:
+            self._dirCacheSizes = {}
+
+            for d in self._dirList:
+               self._dirCacheSizes[d] = get_dir_size(d)
+
+            self._dirLastCheckDate = datetime.now()
+
+         reading.dir_sizes = self._dirCacheSizes
+
+# call main
+#if __name__ == '__main__':
+#   sensor = SystemSensor(True, ["/var/log"])
+#   d = sensor.readData()
+#   print(d.getDict()) 
